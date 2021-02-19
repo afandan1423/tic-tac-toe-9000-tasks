3af9d4bf994a3c6b18d7ed6bdd8727ad23cc4fb3
afandan1423 <53346379+afandan1423@users.noreply.github.com>
2021-02-19 09:12:39 +0300

    Draw
    Now this code is with "draw"

--- game_engine/tic_tac_toe_game.py
+++ game_engine/tic_tac_toe_game.py
@@ -1,6 +1,5 @@
-from typing import List, Callable
 from copy import deepcopy
-
+from typing import Callable, List, Optional
 from .tic_tac_toe_common_lib import TicTacToeTurn, TicTacToeGameInfo, AbstractTicTacToeGame
 
 class TicTacToeGame(AbstractTicTacToeGame):
@@ -15,6 +14,7 @@ class TicTacToeGame(AbstractTicTacToeGame):
         self.__first_player_id = first_player_id
         self.__second_player_id = second_player_id
         self.__winner_id = ""
+        self.__draw = "False"
         self.__strategy = strategy
         self.__turns: List[TicTacToeTurn] = []
 
@@ -59,7 +59,8 @@ class TicTacToeGame(AbstractTicTacToeGame):
             sequence_of_turns=deepcopy(self.__turns),
             first_player_id=self.__first_player_id,
             second_player_id=self.__second_player_id,
-            winner_id=self.__winner_id
+            winner_id=self.__winner_id,
+            draw=self.__draw
         )
         for turn in self.__turns:
             if turn.player_id == self.__first_player_id:
@@ -67,25 +68,45 @@ class TicTacToeGame(AbstractTicTacToeGame):
             else:
                 ch = "O"
             result.field[turn.x_coordinate][turn.y_coordinate] = ch
-        if result.winner_id == "":
-            for a in range(len(result.field)):
-                x = [""] * len(result.field)
-                for y in range(len(x)):
-                    x[y] = result.field[y][a]
+        if result.winner_id == "" and draw == "False":
+            draw_check = [""] * 8
+            y = result.field
+            for a in range(len(y)):
+                x = [""] * len(y)
+                for l in range(len(x)):
+                    x[l] = y[l][a]
                 if x.count(" ") == 0:
                     if x[0] == "X":
                         result.winner_id = result.first_player_id
                     elif x[0] == "O":
                         result.winner_id = result.second_player_id
-                elif result.field[a].count(" ") == 0:
-                    if result.field[a][0] == "X":
+                elif y[a].count(" ") == 0:
+                    if y[a][0] == "X":
                         result.winner_id = result.first_player_id
-                    elif result.field[a][0] == "O":
+                    elif y[a][0] == "O":
                         result.winner_id = result.second_player_id
-            x = result.field
-            if x[0][0] == x[1][1] == x[2][2] or x[0][0] == x[1][1] == x[2][2]:
-                if x[1][1] == "X":
+                if x.count("X") > 0 and x.count("O") > 0:
+                    draw_check[a*2] = "True"
+                if y[a].count("X") > 0 and y[a].count("O") > 0:
+                    draw_check[a*2 + 1] = "True"
+            m, n = [""] * len(y), [""] * len(y)
+            for k in range(len(y)):
+                m[k] = y[k][k]
+                n[k] = y[k][len(y) - k]
+            if m.count("") == 0:
+                if m[1] == "X":
                     result.winner_id = result.first_player_id
-                elif x[1][1] == "O":
+                elif m[1] == "O":
                     result.winner_id = result.second_player_id
+                elif m.count("X") > 0 and m.count("O") > 0:
+                    draw_check[7] = "True"
+            if n.count("") == 0:
+                if n[1] == "X":
+                    result.winner_id = result.first_player_id
+                elif n[1] == "O":
+                    result.winner_id = result.second_player_id
+                elif n.count("X") > 0 and n.count("O") > 0:
+                    draw_check[8] = "True"
+            if draw_check.count("True") == 8:
+                result.draw = "True"
         return result
